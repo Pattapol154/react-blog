254c94054a1fac7df542dc5cea6a3f491ee24912
Pattapol154 <sunfly-210@hotmail.com>
2020-08-09 02:11:07 +0700

    Updates

---
+++ precache-manifest.a8e654ad50048ee155b9c99867e45e75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2816bd90d5c2d0d21415fe601388f6db",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "e2feed2c4987e6108601",
+    "url": "/react-blog/static/js/2.3b10244b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-blog/static/js/2.3b10244b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb1e9814306501a20be",
+    "url": "/react-blog/static/js/main.2514bb9a.chunk.js"
+  },
+  {
+    "revision": "bbc865647cb9d2572353",
+    "url": "/react-blog/static/js/runtime-main.87a5bf9f.js"
+  }
+]);
